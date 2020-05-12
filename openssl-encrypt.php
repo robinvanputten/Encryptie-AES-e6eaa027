79f557afb6654468d0557ff71614ae536d1581a4
redlolz <robinlego@hotmail.nl>
2020-05-12 15:04:58 +0200

    Uitwerking van de exercise

--- openssl-encrypt.php
+++ openssl-encrypt.php
@@ -1,16 +1,18 @@
 <?php
-function encrypt_aes128gcm($plaintext, $key) {
+function encrypt_aes128gcm($plaintext, $key) 
+{
     $cipher = "aes-128-gcm";
     $ivlen = openssl_cipher_iv_length($cipher);
     $iv = openssl_random_pseudo_bytes($ivlen);
-    $ciphertext = openssl_encrypt($plaintext, $cipher, $key, $options=0, $iv, $tag);
+    $ciphertext = openssl_encrypt($plaintext, $cipher, $key, $options = 0, $iv, $tag);
     return [$ciphertext, $iv, $tag];
 }
 
-function decrypt_aes128gcm($ciphertext, $key, $iv, $tag) {
+function decrypt_aes128gcm($ciphertext, $key, $iv, $tag) 
+{
     $cipher = "aes-128-gcm";
     $ivlen = openssl_cipher_iv_length($cipher);
-    $original_plaintext = openssl_decrypt($ciphertext, $cipher, $key, $options=0, $iv, $tag);
+    $original_plaintext = openssl_decrypt($ciphertext, $cipher, $key, $options = 0, $iv, $tag);
     return $original_plaintext;
 }
 
